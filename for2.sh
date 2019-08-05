7016f27e8b5ec1ff93824cc6857c769bdc810107
root <root@ip-172-31-19-135.us-east-2.compute.internal>
2019-08-05 14:42:08 +0000

    Reading Array Variable

---
+++ for2.sh
@@ -0,0 +1,15 @@
+ColorList=("Blue Green Pink White Red")
+for color in $ColorList
+do
+if [ $color == 'Pink' ]
+then
+echo "My favorite color is $color"
+fi 
+done
+for color in $ColorList
+do
+if [ $color == 'Blue' ]
+then
+echo "My favorite color is $color"
+fi
+done
