188666c372e3d445334bca4e629fc0569b7e8835
root <root@ip-172-31-19-135.us-east-2.compute.internal>
2019-08-05 14:50:47 +0000

    Finding odd and even number

---
+++ for4.sh
@@ -0,0 +1,19 @@
+for (( n=1; n<=5; n++ ))
+do
+if (( $n%2==0 ))
+then
+echo "$n is even"
+else
+echo "$n is odd"
+fi
+done
+for (( n=6; n<=10; n++ ))
+do
+if (( $n%2==0 ))
+then
+echo "$n is even"
+else
+echo "$n is odd"
+fi
+done
+
