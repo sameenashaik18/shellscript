0d24ed1e75fa8582483deee508192feb02ce44dc
root <root@ip-172-31-19-135.us-east-2.compute.internal>
2019-08-05 14:25:22 +0000

    infinite loop

---
+++ while5.sh
@@ -0,0 +1,19 @@
+n=1
+while :
+do
+         printf "The current value of n=$n\n"
+         if [ $n == 3 ]
+         then
+                   echo "good"
+         elif [ $n == 5 ]
+         then
+                  echo "bad"
+         elif [ $n == 7 ]
+         then
+                  echo "ugly"
+         elif [ $n == 10 ]
+         then
+                   exit 0
+         fi
+         ((n++))
+done
